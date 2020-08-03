5cd160e92ca4cfbfe60da1c9564de82590e676c8
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-08-03 19:19:21 -0300

    server changed

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 let conexao = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "mysql",
+  password: "Root123@",
   database: "patas",
   port: "3306",
 });
